2e943f4e4fedd4e6c4a7702ed4f579c08c23ec62
MEClemenkowff <52529026+MEClemenkowff@users.noreply.github.com>
2025-03-24 12:28:20 +0100

    Add MySQL support

--- car_rental/car_rental/settings.py
+++ car_rental/car_rental/settings.py
@@ -82,7 +82,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'my_database',
+    #    'USER': 'my_user',
+    #    'PASSWORD': 'my_password',
+    #    'HOST': 'localhost',
+    #    'PORT': '3306',
+    #}
 }
 
 
